dc2d98963bc834a7909fb5ac4ffa161a90c427b5
LHS <jkhk7788@gmail.com>
2022-03-31 22:19:35 +0900

    u

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const config = {
-    viewPath :path.join(__dirname,'/Front/views/'),
+    viewPath :path.join(__dirname,'/front/'),
     port:4001,
 }
 module.exports = config;
