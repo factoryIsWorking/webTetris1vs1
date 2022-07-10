ecba321ee55feefe54ad986c4088118e5bd906a0
LeeHanSeong7 <jkhk7788@gmail.com>
2022-07-10 17:45:07 +0900

    update
    update

--- Web/back/index.js
+++ Web/back/index.js
@@ -1,25 +1,24 @@
-global.CONFIG = require('./config');
-const cors = require('cors');
+global.CONFIG = require("./config");
+const cors = require("cors");
 const Express = {
-    express : require('express'),
-    applyTo : function(app){
-        const exp = this.express;
-        app.use('/',exp.static(global.CONFIG.publicPath));
-        app.use(exp.urlencoded({extended:true}));
-        app.use(exp.json());
-    }
+	express: require("express"),
+	applyTo: function (app) {
+		const exp = this.express;
+		app.use("/", exp.static(global.CONFIG.publicPath));
+		app.use(exp.urlencoded({ extended: true }));
+		app.use(exp.json());
+	},
 };
 const app = new Express.express();
 Express.applyTo(app);
 app.use(cors());
 let port = process.env.PORT;
-if (port == null || port == "")
-    port = global.CONFIG.port;
+if (port == null || port == "") port = global.CONFIG.port;
 
-(async ()=>{
-    require('./API/init')(app);
-    require('./services/DB/init')().then(_=>console.log('DB service ready'));
-    const Server = await app.listen(port, null);
-    require('./services/webSocket/init')(Server);
-    return Server;
-})().then(()=>console.log(`App listening on port ${port} : http://localhost:${port}`));
+(async () => {
+	require("./API/init")(app);
+	const Server = await app.listen(port, null);
+	return Server;
+})().then(() =>
+	console.log(`App listening on port ${port} : http://localhost:${port}`)
+);
--- Web/back/services/dataObject/Games.js
+++
@@ -1,3 +0,0 @@
-const Games = {};
-
-export default Games;
